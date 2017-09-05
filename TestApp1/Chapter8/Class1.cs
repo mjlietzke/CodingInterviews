2006539df7828c1678e589f3bb8378fbdbbba4bd
Matt <mjlietzke@gmail.com>
2017-09-04 20:31:51 -0700

    initial implementation of problem 1

--- TestApp1/Chapter8/Class1.cs
+++ TestApp1/Chapter8/Class1.cs
@@ -7,7 +7,37 @@ using System.Threading.Tasks;
 namespace InterviewQuestions.Chapter8 {
 	class Chapter8Problem1 : ICodingProblem {
 		public void Run() {
-			throw new NotImplementedException();
+			int stairs = 3;
+			System.Console.Out.WriteLine($"For {stairs} number of stairs, there are {CountingStairs(stairs)} ways to climb them");
+
 		}
+
+		//child is running up the N stairs either by 1 2 ir 3
+		//imlpement a method that counts all possible ways that a child can climb the stairs
+
+		//basic premise:
+		//we can take the remaining number of stairs at any given level and count the invocation of the funciton when taking 1 2 or 3 steps and repeat
+
+		public int CountingStairs(int numberOfStairsLeft) {
+			int count = 0;
+			if(0 == numberOfStairsLeft) {
+				return 0;
+			}
+			if (numberOfStairsLeft >= 3) {
+				//numberOfStairsLeft = numberOfStairsLeft - 3;
+				count += 1 + CountingStairs(numberOfStairsLeft - 3);
+			}
+			if (numberOfStairsLeft >= 2) {
+				//numberOfStairsLeft = numberOfStairsLeft - 2;
+				count += 1 + CountingStairs(numberOfStairsLeft - 2);
+			}
+			if (numberOfStairsLeft >= 1) {
+				//numberOfStairsLeft = numberOfStairsLeft - 1;
+				count += 1 + CountingStairs(numberOfStairsLeft - 1);
+			}
+
+			return count;
+		}
+	
 	}
 }
