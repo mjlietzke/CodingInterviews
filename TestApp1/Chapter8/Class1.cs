3037a2fff218f051982a2f8cc95b2c37b9f6cabe
Matt <mjlietzke@gmail.com>
2017-09-04 20:19:00 -0700

    Added initial chapter 8 folder and problem 1

---
+++ TestApp1/Chapter8/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions.Chapter8 {
+	class Chapter8Problem1 : ICodingProblem {
+		public void Run() {
+			throw new NotImplementedException();
+		}
+	}
+}
